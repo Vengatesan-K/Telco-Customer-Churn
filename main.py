ed610e62bf83b7c16abd0e8a3e7a9eb6d6f721dc
Vengatesan-K <kannanvenkatesh772@gmail.com>
2024-03-23 16:31:27 +0530

    Data Ingestion Added

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from src.CustomerChurn.logging import logger
+from src.CustomerChurn.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to the customer churn prediction")
+
+
+STAGE_NAME = "Data Ingestion stage"
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
